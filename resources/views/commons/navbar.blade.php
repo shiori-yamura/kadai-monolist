96c6c285a10f917037034315af237c599ad36bd9
EC2 Default User <ec2-user@ip-172-31-21-245.us-east-2.compute.internal>
2018-05-15 11:31:28 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="" class="navbar-left"><img src="{{ secure_asset('images/logo.png') }}" alt="monolist"></a>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="navbar navbar-nav navbar-right">
+                        <li><a href="#">新規登録</a></li>
+                        <li><a href="#">ログイン</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
