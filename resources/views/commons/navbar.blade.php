c824800c85935b184cea385883954dab35d77a80
EC2 Default User <ec2-user@ip-172-31-21-245.us-east-2.compute.internal>
2018-05-19 10:37:23 +0000

    ranking have

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -22,6 +22,7 @@
                         </a>
                         <ul class="dropdown-menu">
                             <li><a href="{{route('ranking.want')}}">Wantランキング</a></li>
+                            <li><a href="{{route('ranking.have')}}">Haveランキング</a></li>
                         </ul>
                     </li>
                     <li class="dropdown">
